c7586c9b85667ff20a0aa739465cbcb8fd514645
zumabektairbekuulu53 <zumabektairbekuulu53@gmail.com>
2025-06-27 12:47:30 -0500

    Add files via upload

---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Driver Statement Site</h1>
+      <p>This is a working demo page deployed without TypeScript.</p>
+    </div>
+  );
+}
